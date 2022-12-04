aab292d5ab4e4d6fe83d73ac11e5b29f845fe1c6
Justin Buckley <justin.buckley@hushmail.com>
2022-12-04 08:27:26 -0400

    Part 2 of Day 4

--- Day_4/index.js
+++ Day_4/index.js
@@ -5,6 +5,9 @@ const rawSections = fs.readFileSync('input.txt').toString().split("\n");
 // Part 1
 let numContainingPairs = 0;
 
+// Part 2
+let allContainingPairs = 0;
+
 const makeString = (originalString) => {
     let split = originalString.split('-');
     let low = parseInt(split[0]);
@@ -25,7 +28,19 @@ const compare = (a, b) => {
     } else if (a.length > b.length) {
         return 1;
     }
-    console.log('-/-/-/ EQUAL!!!!', a, b);
+    return 0;
+}
+
+const findAllPairs = (first, second) => {
+    let splitFirst = first.split(",");
+    let splitSecond = second.split(",");
+    
+    for (let item of splitFirst) {
+        if ((splitSecond.indexOf(item) >= 0) && (item != "")) {
+            console.log('Found!', splitSecond.indexOf(item));
+            return 1;
+        } 
+    }
 
     return 0;
 }
@@ -38,10 +53,17 @@ for (let sec of rawSections) {
     converted.push(firstRange, secondRange);
     converted.sort(compare); // Sort array so that shorter strings are first
 
+    // for part 1
     if ((converted[1].indexOf(converted[0])) >= 0) {
         numContainingPairs += 1;
     }
+
+    // for part 2
+    allContainingPairs = allContainingPairs + findAllPairs(converted[0], converted[1]);
 }
 
 // Part 1 result
 console.log(`Part 1: The number of assignment pairs where one range is contained with the other: ${numContainingPairs}`);
+
+// Part 2 result
+console.log(`Part 2: The number of overlapping assignment pairs is ${allContainingPairs}`);
