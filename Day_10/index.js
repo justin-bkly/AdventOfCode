1ab285e19620d06bbdcd84861473147d986595d8
Justin Buckley <justin.buckley@hushmail.com>
2022-12-12 10:10:49 -0400

    Day 10 part 2

--- Day_10/index.js
+++ Day_10/index.js
@@ -3,10 +3,27 @@ const raw = fs.readFileSync('input.txt').toString().split("\n");
 
 let x = 1;
 let cycleHistory = [[]];
+let pixels = '';
 
+const writePixels = () => {
+    let cycle = cycleHistory.length;
+    let cycleMod = cycle % 40;
+
+    if ((x == cycleMod || (x + 1) == cycleMod || (x + 2) == cycleMod)) {
+        pixels = pixels + "#";
+    } else {
+        pixels = pixels + ".";
+    }
+
+    if (cycle % 40 == 0) {
+        pixels = pixels + "\n";
+    }
+}
+ 
 const runCycles = (numCycles) => {
     for (let cycle = 1; cycle <= numCycles; cycle++) {
         let entry = [x, cycleHistory.length * x];
+        writePixels();
         cycleHistory.push(entry);
     }
 }
@@ -22,5 +39,9 @@ for (let row of raw) {
     }
 }
 
+// Part 1
 const cyclesSum = cycleHistory[20][1] + cycleHistory[60][1] + cycleHistory[100][1] + cycleHistory[140][1] + cycleHistory[180][1] + cycleHistory[220][1];
-console.log(cyclesSum);
+console.log(`Part 1: Sum of cycles is ${cyclesSum}`);
+
+// Part 2
+console.log(`Part 2:\n`,pixels);
